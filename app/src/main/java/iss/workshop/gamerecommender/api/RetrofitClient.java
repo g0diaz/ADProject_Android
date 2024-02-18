64586696f4ff406384a2852e98e6bf36d2313e0c
Yu Shen <lukeshnno1@gmail.com>
2024-02-18 13:46:02 +0800

    fixed

--- app/src/main/java/iss/workshop/gamerecommender/api/RetrofitClient.java
+++ app/src/main/java/iss/workshop/gamerecommender/api/RetrofitClient.java
@@ -5,8 +5,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-//    public static final String BASE_URL = "http://10.0.2.2:8080/";
-    public static final String BASE_URL = "http://146.190.82.15:8080/";
+    public static final String BASE_URL = "http://10.0.2.2:8080/";
+//    public static final String BASE_URL = "http://146.190.82.15:8080/";
     private Retrofit retrofit;
     public RetrofitClient () {
         retrofit = new Retrofit.Builder()
